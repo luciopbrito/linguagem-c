4c3078f63dab6d325f1a9f21c8743cda793e1e21
Lúcio Brito <luciopereirabritoo@gmail.com>
2022-10-11 21:08:53 -0300

    exemplo 2 aula08

---
+++ aulas-liguagem-c/aula08_ex2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int contador;
+	
+	for(contador=0; contador<=100;contador++)
+	{
+		printf("%d \n", contador);
+	}
+	
+	return 0;
+}
